ba4eb922023526b0b214910c88ba653ff2ad2e4e
Webster Cheng <chengwe@os2.engr.oregonstate.edu>
2018-04-10 23:53:32 -0700

    Concurrency file(rdrand)\ fix make file

--- concurrency/con1/con.c
+++ concurrency/con1/con.c
@@ -1,102 +1,164 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "mt19937ar.h"
 
 #define Buffer_Limit 10
 
 int Buffer_Index_Value = 0;
-char *Buffer_Queue;
-
-int queue[32] = {};
+int isRdrand = 0;
 int head = 0;
 int tail = 0;
 int value = 0;
+char *Buffer_Queue;
 
-pthread_mutex_t mutex_variable = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t Buffer_Queue_Not_Full = PTHREAD_COND_INITIALIZER;
-pthread_cond_t Buffer_Queue_Not_Empty = PTHREAD_COND_INITIALIZER;
-
-void queue_insert_value(int i)
+struct value
 {
-    if(tail == 32)
+    int val1;
+    int val2;
+} queue[32];
+
+pthread_mutex_t mutexVal;
+pthread_cond_t condc, condp;
+
+void queueInsValues(int val1, int val2)
+{
+    if (tail == 32)
         tail = 0;
-    queue[tail] = i;
+    queue[tail].val1 = val1;
+    queue[tail].val2 = val2;
     tail++;
     value++;
 }
 
-int queue_pop_value()
+struct value *queuePopValues()
 {
     if (head == 32)
         head = 0;
     value--;
-    return queue[head++];
+    return &(queue[head++]);
+}
+
+int genmt19937(int min, int max)
+{
+    init_genrand(time(NULL));
+    // min to max
+    //1 ~ 9
+    int x = (int)genrand_int32();
+    return (abs(x) % (max - 1)) + min;
+}
+
+int rdrandNumber(int min, int max)
+{
+    unsigned int gen;
+    unsigned char isOk;
+
+    asm volatile("rdrand %0; setc %1"
+                 : "=r"(gen), "=qm"(isOk));
+
+    // 1 = success, 0 = underflow
+    if (isOk)
+        return ((gen % max) + min);
+    else
+        return 0;
+}
+
+void checkRdrand()
+{
+    unsigned int eax, ebx, ecx, edx;
+    eax = 0x01;
+    __asm__ __volatile__(
+        "cpuid;"
+        : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+        : "a"(eax));
+
+    isRdrand = ecx & 0x40000000 ? 1 : 0;
+}
+
+int randNumber(int min, int max)
+{
+    return isRdrand ? rdrandNumber(min, max) : genmt19937(min, max);
 }
 
 void *Consumer()
 {
     int i = 0;
-    while(i<3)
+    while (i < 33)
     {
-        /*
-        pthread_mutex_lock(&mutex_variable);
-        if(Buffer_Index_Value == -1)
-        {            
-            pthread_cond_wait(&Buffer_Queue_Not_Empty, &mutex_variable);
-        }                
-        printf("Consumer:%d\t", Buffer_Index_Value--);
-        pthread_mutex_unlock(&mutex_variable);
-        pthread_cond_signal(&Buffer_Queue_Not_Full);
-        */
-        pthread_mutex_lock(&mutex_variable);
-        if(value <= 0)
-            pthread_cond_wait(&Buffer_Queue_Not_Empty, &mutex_variable);
+        pthread_mutex_lock(&mutexVal);
+        if (value == 0)
+        {
+            printf("Consumer is waiting!!\n");
+            pthread_cond_wait(&condc, &mutexVal);
+        }
+        struct value *x = queuePopValues();
+        printf("Consumer:%d\n", x->val1);
+        //2-9
+        sleep(randNumber(2, 9));
+        printf("Consumer:%d\n", x->val2);
 
-        printf("Consumer:%d\n", queue_pop_value());
-        pthread_mutex_unlock(&mutex_variable);
-        pthread_cond_signal(&Buffer_Queue_Not_Full);
+        /* wake up consumer */
+        /* release the buffer */
+        pthread_cond_signal(&condp);
+        pthread_mutex_unlock(&mutexVal);
         i++;
-    }    
+    }
+
+    pthread_exit(0);
 }
 
 void *Producer()
-{    
-    int i =0;
-    while(i<3)
-    { 
-        /*
-        pthread_mutex_lock(&mutex_variable);
-        if(Buffer_Index_Value == Buffer_Limit)                         
-            pthread_cond_wait(&Buffer_Queue_Not_Full, &mutex_variable);
-                                
-        Buffer_Queue[Buffer_Index_Value++] = '@';
-        printf("Producer:%d\t", Buffer_Index_Value);
-        pthread_mutex_unlock(&mutex_variable);
-        pthread_cond_signal(&Buffer_Queue_Not_Empty);        
-        */
-        pthread_mutex_lock(&mutex_variable);
-        if(value == 32)
-            pthread_cond_wait(&Buffer_Queue_Not_Full, &mutex_variable);
+{
+    int i = 0;
+    while (i < 33)
+    {
+        pthread_mutex_lock(&mutexVal);
+        if (value == 32)
+        {
+            printf("Producer is waiting!\n");
+            pthread_cond_wait(&condp, &mutexVal);
+        }
 
-        queue_insert_value(i);
-        printf("Producer:%d\n", i);
-        pthread_mutex_unlock(&mutex_variable);
-        pthread_cond_signal(&Buffer_Queue_Not_Empty);
+        //1-9
+        int val1 = randNumber(1, 10);
+        sleep(randNumber(1, 5));
+        int val2 = randNumber(1, 10);
+        queueInsValues(val1, val2);
+
+        printf("Producer:%d, %d\n", val1, val2);
+        sleep(3);
+        //3-7 
+
+        /* wake up consumer */
+        /* release the buffer */
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&mutexVal);
         i++;
-    }    
+    }
+    pthread_exit(0);
 }
 
 int main()
-{  
-    /*
-    queue_insert_value(10);
-    queue_pop_value();
-    */
-    pthread_t producer_thread_id, consumer_thread_id;
-    Buffer_Queue = (char *) malloc(sizeof(char) * Buffer_Limit);            
-    pthread_create(&producer_thread_id, NULL, Producer, NULL);
-    pthread_create(&consumer_thread_id, NULL, Consumer, NULL);
-    pthread_join(producer_thread_id, NULL);
-    pthread_join(consumer_thread_id, NULL);
+{
+    srand(time(NULL));
+
+    pthread_mutex_init(&mutexVal, NULL);
+    pthread_cond_init(&condc, NULL); /* Initialize consumer condition variable */
+    pthread_cond_init(&condp, NULL); /* Initialize producer condition variable */
+
+    pthread_t producerThread, consumerThread;
+    pthread_create(&producerThread, NULL, Producer, NULL);
+    pthread_create(&consumerThread, NULL, Consumer, NULL);
+
+    pthread_join(producerThread, NULL);
+    pthread_join(consumerThread, NULL);
+
+    /* Free up the_mutex */
+    /* Free up consumer condition variable */
+    /* Free up producer condition variable */
+    pthread_mutex_destroy(&mutexVal);
+    pthread_cond_destroy(&condc);
+    pthread_cond_destroy(&condp);
+
     return 0;
 }
